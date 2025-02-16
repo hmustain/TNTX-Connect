df7f2063b8ddec8dab59159b112d27c6214bc9ab
hmustain <mustain05@aim.com>
2025-02-16 15:21:13 -0600

    added middlware protection to chat routes

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const { createChat, getChatsByTicket } = require('../controllers/chatController');
+const { protect } = require('../middleware/auth');
 
 // Create a chat message
-router.post('/', createChat);
+router.post('/', protect, createChat);
 
 // Get chat messages by ticket ID
-router.get('/ticket/:ticketId', getChatsByTicket);
+router.get('/ticket/:ticketId', protect, getChatsByTicket);
 
 module.exports = router;

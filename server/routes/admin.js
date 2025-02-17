4fab80c6fb272fc70c7d8bf10464157ce9a4945b
hmustain <mustain05@aim.com>
2025-02-17 17:48:58 -0600

    added admin.js route to define permissions of each user

---
+++ server/routes/admin.js
@@ -0,0 +1,32 @@
+// server/routes/admin.js
+const express = require('express');
+const router = express.Router();
+const { 
+  createUser,
+  updateUser,
+  deleteUser,
+  changeUserRole,
+  setUserActiveStatus,
+  createCompany,
+  updateCompany,
+  deleteCompany 
+} = require('../controllers/adminController');
+const { protect } = require('../middleware/auth');
+const { authorize } = require('../middleware/authorize');
+
+// All admin routes are protected and require the admin role.
+router.use(protect, authorize('admin'));
+
+// User management routes
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+router.put('/users/:id/role', changeUserRole);
+router.put('/users/:id/status', setUserActiveStatus);
+
+// Company management routes
+router.post('/companies', createCompany);
+router.put('/companies/:id', updateCompany);
+router.delete('/companies/:id', deleteCompany);
+
+module.exports = router;

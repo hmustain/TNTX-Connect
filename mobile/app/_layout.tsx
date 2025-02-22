e2596ed85980584e94a6ea838e1a9dc9c7051391
hmustain <mustain05@aim.com>
2025-02-21 20:43:54 -0600

    setting up framework for mobile app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,9 @@
+import React, { useEffect } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
+import { Slot } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -29,10 +27,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
---
+++ mobile/components/LandingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const LandingScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
+      <View>
+        <Text>Landing Screen for TNTX Mobile</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LandingScreen;

d8fc6fbdfc88ccf7cf5db9b4ec51fb64fff25c03
hmustain <mustain05@aim.com>
2025-03-04 19:29:02 -0600

    bringing ChatScreen.css into ticketscreen.js

--- client/src/components/TicketScreen.js
+++ client/src/components/TicketScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import "../ChatScreen.css";
 
 const TicketScreen = () => {
   const { id } = useParams();
